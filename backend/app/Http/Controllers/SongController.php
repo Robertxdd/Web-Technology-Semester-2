e5ef4f5071d58c85a31f9c2fe75ee3c7055cc5f1
Robert <robert820404@gmail.com>
2025-11-16 17:32:26 +0100

    i added comments for the song controller to see whats for admin and whats for user

--- backend/app/Http/Controllers/SongController.php
+++ backend/app/Http/Controllers/SongController.php
@@ -7,25 +7,25 @@ use Illuminate\Http\Request;
 
 class SongController extends Controller
 {
-    public function index() {
+    public function index() {  // normal user
         return Song::all();
     }
 
-    public function store(Request $req) {
+    public function store(Request $req) { // admin user 
         return Song::create($req->all());
     }
 
-    public function show($id) {
+    public function show($id) { // normal user
         return Song::findOrFail($id);
     }
 
-    public function update(Request $req, $id) {
+    public function update(Request $req, $id) { // admin user  
         $song = Song::findOrFail($id);
         $song->update($req->all());
         return $song;
     }
 
-    public function destroy($id) {
+    public function destroy($id) { // admin user 
         Song::destroy($id);
         return response()->noContent();
     }
